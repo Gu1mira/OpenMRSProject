9c9c38b078d225557e137478fe144c88941b23c1
Muharrem Yurtsever <muharremyurtsever@gmail.com>
2024-03-25 23:12:01 +0000

    I updated TC_407.java and TC_407_Elements.java

--- src/US_407/TC_407.java
+++ src/US_407/TC_407.java
@@ -1,4 +1,55 @@
 package US_407;
 
-public class TC_407 {
+import TC_409.Utilities.BaseDriver;
+import TC_409.Utilities.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Random;
+
+public class TC_407 extends BaseDriver {
+    @Test
+    public void TC_407(){
+
+        TC_407_Elements TTT=new TC_407_Elements();
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        wait.until(ExpectedConditions.urlToBe("https://demo.openmrs.org/openmrs/login.htm"));
+
+        WebElement email=driver.findElement(By.id("username"));
+        email.sendKeys("admin");
+
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("Admin123");
+        TTT.selectLocation.click();
+
+        WebElement loginBtn=driver.findElement(By.id("loginButton"));
+        loginBtn.click();
+
+        wait.until(ExpectedConditions.titleIs("Home"));
+        System.out.println("Login is finished.");
+
+        wait.until(ExpectedConditions.titleIs("Home"));
+        wait.until(ExpectedConditions.elementToBeClickable(TTT.findPatient));
+        TTT.findPatient.click();
+        wait.until(ExpectedConditions.elementToBeClickable(TTT.searchBox));
+        TTT.searchBox.sendKeys("100K18" + Keys.ENTER);
+        wait.until(ExpectedConditions.elementToBeClickable(TTT.searchResult));
+        TTT.searchResult.click();
+        wait.until(ExpectedConditions.elementToBeClickable(TTT.deletePatient));
+        TTT.deletePatient.click();
+
+
+
+
+
+
+
+
+    }
 }
---
+++ src/US_407/TC_407_Elements.java
@@ -0,0 +1,34 @@
+package US_407;
+
+import TC_409.Utilities.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class TC_407_Elements extends BaseDriver {
+    public TC_407_Elements(){
+        PageFactory.initElements(BaseDriver.driver,this);
+    }
+
+    @FindBy(xpath = "//*[@id=\"Inpatient Ward\"]")
+    public WebElement selectLocation;
+
+    @FindBy(css = "[class='icon-search']")
+    public WebElement findPatient;
+
+    @FindBy(xpath = "//*[@id=\"patient-search\"]")
+    public WebElement searchBox;
+
+    @FindBy(css = "[class='recent-lozenge']")
+    public WebElement searchResult;
+
+    @FindBy(xpath = "//div[contains(text(),'Delete Patient')]")
+    public WebElement deletePatient;
+
+    @FindBy(xpath = "//div[contains(text(),'Delete Patient')]")
+    public WebElement confirmButton;
+
+}
