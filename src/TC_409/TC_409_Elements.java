d3d64b6e471d0d00c014826d0df687af5f54e778
gu1mira <sidagulova@gmail.com>
2024-03-27 17:50:32 +0200

    TC_409 is added

---
+++ src/TC_409/TC_409_Elements.java
@@ -0,0 +1,16 @@
+package TC_409;
+
+import Utilities.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC_409_Elements {
+    public TC_409_Elements(){
+        PageFactory.initElements(BaseDriver.driver,this);
+    }
+
+
+    @FindBy(xpath = "//*[@class='icon-user']")
+    public WebElement registerPatient;
+}
